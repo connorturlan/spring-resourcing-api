86fbbd1ff51815586bacfbd49e0ca83f62a19149
Connor <connorturlan@gmail.com>
2022-10-19 12:51:02 +1030

    inital commit

---
+++ src/main/java/resourcingapi/connorturlan/com/au/ResourcingApiApplication.java
@@ -0,0 +1,13 @@
+package resourcingapi.connorturlan.com.au;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/resourcingapi/connorturlan/com/au/ResourcingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package resourcingapi.connorturlan.com.au;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
