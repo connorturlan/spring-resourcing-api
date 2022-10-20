6f5e5180105668bb3158aa3d5155750026a21156
Connor <connorturlan@gmail.com>
2022-10-20 11:10:59 +1030

    added getAll to JobServince

--- src/main/java/resourcingapi/connorturlan/com/au/Job/JobService.java
+++ src/main/java/resourcingapi/connorturlan/com/au/Job/JobService.java
@@ -1,5 +1,19 @@
 package resourcingapi.connorturlan.com.au.Job;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class JobService {
-	
+	@Autowired
+	private JobRepository repository;
+
+	public List<Job> GetAll() {
+		return repository.findAll();
+	}
 }
