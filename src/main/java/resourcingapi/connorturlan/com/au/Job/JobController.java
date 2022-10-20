90599b4b06ccb6ca407903047d0f2e51713b0321
Connor <connorturlan@gmail.com>
2022-10-20 11:13:24 +1030

    Added JobController

--- src/main/java/resourcingapi/connorturlan/com/au/Job/JobController.java
+++ src/main/java/resourcingapi/connorturlan/com/au/Job/JobController.java
@@ -1,7 +1,9 @@
 package resourcingapi.connorturlan.com.au.Job;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/jobs")
 public class JobController {
+	@Autowired
+	private JobService service;
+
 	@GetMapping
-	public ResponseEntity<ArrayList<Job>> GetAll() {
-		return null;
+	public ResponseEntity<List<Job>> GetAll() {
+		List<Job> jobs = service.GetAll();
+		return new ResponseEntity<>(jobs, HttpStatus.OK);
 	}
 }
