3ce92837a1bd807a25c6806866c065ca437aa900
Connor <connorturlan@gmail.com>
2022-10-23 09:45:00 +1030

    added sorting to find assigned/unassigned jobs

--- src/main/java/resourcingapi/connorturlan/com/au/Job/JobController.java
+++ src/main/java/resourcingapi/connorturlan/com/au/Job/JobController.java
@@ -1,7 +1,9 @@
 package resourcingapi.connorturlan.com.au.Job;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import resourcingapi.connorturlan.com.au.Temp.Temp;
@@ -29,6 +33,15 @@ public class JobController {
 	private TempService tempService;
 
 	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Job>> HandleGet(@RequestParam(required = false) Boolean assigned) {
+		// check if there is a param to parse.
+		if (assigned != null) {
+			return FindFilterAssigned(assigned);
+		}
+		return FindAll();
+	}
+	
 	public ResponseEntity<List<Job>> FindAll() {
 		List<Job> jobs = jobService.FindAll();
 		return new ResponseEntity<>(jobs, HttpStatus.OK);
@@ -80,5 +93,12 @@ public class JobController {
 
 		// otherwise return the requested job.
 		return new ResponseEntity<>(maybeJob.get(), HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<List<Job>> FindFilterAssigned(boolean assigned) {		
+		List<Job> allJobs = jobService.FindAll();
+		List<Job> jobs = allJobs.stream().filter(job -> (job.getTemp() != null) == assigned).collect(Collectors.toList());
+
+		return new ResponseEntity<>(jobs, HttpStatus.OK);
 	}
 }
