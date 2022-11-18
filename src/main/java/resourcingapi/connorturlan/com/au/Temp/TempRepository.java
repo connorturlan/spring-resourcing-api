ca2b6e8fb602f0782c3cb7dba7aa6e6002f2620e
Connor <connorturlan@gmail.com>
2022-11-18 10:54:25 +1030

    clean up

--- src/main/java/resourcingapi/connorturlan/com/au/Temp/TempRepository.java
+++ src/main/java/resourcingapi/connorturlan/com/au/Temp/TempRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TempRepository extends JpaRepository<Temp, Long> {
-	@Query("select t from Temp t left join Job j on t.id = j.temp.id WHERE j IS NULL OR NOT ((?1 <= j.startDate AND ?1 >= j.endDate) OR (?2 <= j.startDate AND ?2 >= j.endDate))")
+	// select all unassigned temps or temps that are free for a given period.
+	@Query("SELECT t FROM Temp t LEFT JOIN Job j ON t.id = j.temp.id WHERE j IS NULL OR NOT ((?1 <= j.startDate AND ?1 >= j.endDate) OR (?2 <= j.startDate AND ?2 >= j.endDate))")
 	List<Temp> FindAvailable(LocalDate jobStart, LocalDate jobEnd);
 }
